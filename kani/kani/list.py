226e97c6b903bdb33d3303185e53a1a39c0f1606
Kanimozhi <kanimozhiyelagiri@gmail.com>
2026-05-13 10:48:07 +0530

    initial python files

--- kani/kani/list.py
+++ kani/kani/list.py
@@ -27,3 +27,22 @@ print(thislist[1])
 
 thislist = ["apple", "banana", "cherry"]
 print(thislist[-1])
+#The search will start at index 2 (included) and end at index 5 (not included)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+#Check if "apple" is present in the list
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+#Change the second item:
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+#Change the second value by replacing it with two new values:
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+#Using the append() method to append an item:
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
