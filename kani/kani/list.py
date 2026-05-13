9c0affa2fa9eb1fcdc0b336f3ca53f3f08bda48b
Kanimozhi <kanimozhiyelagiri@gmail.com>
2026-05-13 10:51:46 +0530

    initial python files

--- kani/kani/list.py
+++ kani/kani/list.py
@@ -45,4 +45,66 @@ print(thislist)
 #Using the append() method to append an item:
 thislist = ["apple", "banana", "cherry"]
 thislist.append("orange")
+#Print all items in the list, one by one  using for loop:
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+  #Print all items, using a while loop to go through all the index numbers
+  thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+  #With list comprehension you can do all that with only one line of code:
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+#Sort the list alphabetically:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
 print(thislist)
+#Sort the list descending:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+#Case sensitive sorting can give an unexpected result:
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+#Reverse the order of the list items:
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+# the copy() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+# using the : (slice) operator
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+#Join two list:
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+#Append list2 into list1:
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+#Use the extend()
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
